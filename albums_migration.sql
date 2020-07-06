8c19a8cbc4c818de3fc9d409b491a48c57660454
George Williams <george412williams@gmail.com>
2020-07-06 14:52:58 -0500

    fixed errors to run script

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,14 +5,9 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums(
     id INT UNSIGNED NOT NULL AUTO-INCREMENT,
     artist VARCHAR(20),
-    name VARCHAR(20),
-    release_date INT DATE NULL,
-    sales FLOAT NULL,
+    name VARCHAR(20) NOT NULL,
+    release_date DATE,
+    sales FLOAT UNSIGNED,
     genre VARCHAR(10),
--- artist — string for storing the recording artist name
--- name — string for storing a record name
--- release_date — integer representing year record was released
--- sales — floating point value for number of records sold (in millions)
--- genre — string for storing the record's genre(s)
-     PRIMARY KEY (id)
+    PRIMARY KEY (id)
 );
