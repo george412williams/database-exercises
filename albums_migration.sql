0809abcf0b00b794c819d3a3e14850a9ee36f384
George Williams <george412williams@gmail.com>
2020-07-06 15:01:56 -0500

    errors fixed table added successfully

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,11 +3,11 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE IF NOT EXISTS albums(
-    id INT UNSIGNED NOT NULL AUTO-INCREMENT,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(20),
     name VARCHAR(20) NOT NULL,
     release_date DATE,
-    sales FLOAT UNSIGNED,
+    sales DECIMAL(10,2),
     genre VARCHAR(10),
     PRIMARY KEY (id)
 );
