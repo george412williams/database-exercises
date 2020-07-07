2511d9020eb0c1a0c58fc5a9e3d462a7a8835dc7
George Williams <george412williams@gmail.com>
2020-07-07 14:43:03 -0500

    added miscelaneous to select exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,11 +1,17 @@
 USE codeup_test_db;
 
-SELECT * from albums where name = 'Pink Floyd';
+SELECT 'Name of P Floyd albums' as 'Ex 1';
+SELECT name from albums where name = 'Pink Floyd';
+SELECT 'Beatles'' Sgt Pepper release date' as 'Ex 2';
 SELECT release_date from albums where name = 'Sgt. Peppers Lonely Hearts Club Band';
+SELECT 'Nevermind Genre' as 'Ex 3';
 SELECT genre from albums where name = 'Nevermind';
-SELECT name from albums where release_date between 1989 and 2000;
+SELECT 'Songs of the 90s' as 'Ex 4';
+SELECT name from albums where release_date between 1990 and 1999;
+SELECT '10 mil category' as 'Ex 5';
 SELECT name from albums where sales < 20;
-SELECT name from albums where genre LIKE '%Rock%';
+SELECT 'Vanilla Rock' as 'Ex 6';
+SELECT name from albums where genre LIKE '%rock%';
 
 
 
