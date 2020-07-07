edecb2582895cd8f67ef30a802f5884031438080
George Williams <george412williams@gmail.com>
2020-07-07 12:03:27 -0500

    part 2 complete

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,16 @@
 USE codeup_test_db;
 
-SELECT
+SELECT * from albums where name = 'Pink Floyd';
+SELECT release_date from albums where name = 'Sgt. Peppers Lonely Hearts Club Band';
+SELECT genre from albums where name = 'Nevermind';
+SELECT name from albums where (1989 > release_year < 2000);
+SELECT name from albums where sales < 20;
+SELECT name from albums where genre LIKE '%Rock%';
+
+
+
+
+
+
+
+
